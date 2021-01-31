dd7e8b621f61534e7804b5753c07eb14453e4288
edcel22 <edcelcalzoestadola@gmail.com>
2021-02-01 00:34:26 +0800

    first commit

---
+++ components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+	<nav class="navbar" role="navigation" aria-label="main navigation">
+		<div class="navbar-brand">
+			<nuxt-link to="/" class="nav-item">
+				<img src="/images/logo.png" alt="Web School logo" class="logo">
+			</nuxt-link>	
+		</div>
+		<div class="navbar-menu">
+			
+		</div>
+	</nav>
+</template>
+<style lang="stylus" scoped>
+	.navbar
+		padding: 10px
+		.navbar-brand
+			.logo
+				width: 80px
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="home" class="default">
+  </div>
+</template>
+<script>
+  import  NavBar from '~/components/NavBar'
+
+  export default {
+    components: {
+      NavBar,
+    }
+  }
+</script>
