8a6f348cc6b675c00387c63f29eb1dcab3ca4568
edcel22 <edcelcalzoestadola@gmail.com>
2021-02-04 07:51:29 +0800

    update the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,59 @@
 <template>
   <div id="home" class="default">
-  	<section class="technologies">
-  		<div class="items">
-  			<div class="item">WordPress</div>
-  		</div>
-  	</section>
+    <div class="page_content">
+      <section class="technologies">
+        <h2>Technologies</h2>
+        <div class="items">
+          <div class="item" v-for="(technology, key) in technologies">
+            <div class="top">
+              <div class="overlay">
+                <div :class="(type === 'Problems' ? 'left' : 'right')" v-for="(type, key) in technology.types">
+                  <nuxt-link to="/">{{type}}</nuxt-link>
+                </div>
+              </div>
+              <img src="/images/wordpress-logo.png" alt="WordPress">
+            </div>
+            <div class="bottom">
+              <p>{{technology.name}}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 <script>
-  import  NavBar from '~/components/NavBar'
-
   export default {
-    components: {
-      NavBar,
+    data () {
+      return {
+        technologies: [
+          {
+            name: "WordPress",
+            types: [
+              'Problems', 'Notes']
+          },
+          {
+            name: "Laravel",
+            types: [
+              'Problems', 'Notes']
+          },
+          {
+            name: "Nuxtjs",
+            types: [
+              'Problems', 'Notes']
+          },
+          {
+            name: "Vuejs",
+            types: [
+              'Problems', 'Notes']
+          },
+          {
+            name: "Server Stuffs",
+            types: [
+              'Problems', 'Notes']
+          },
+        ]
+      }
     }
   }
 </script>
